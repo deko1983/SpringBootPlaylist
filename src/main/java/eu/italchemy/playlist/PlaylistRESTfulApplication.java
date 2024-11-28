52149b4f1900b46dad60abfd081219d0f7cbf66a
deko1983 <dekoatwork@gmail.com>
2024-11-28 10:26:51 +0100

    skeleton

---
+++ src/main/java/eu/italchemy/playlist/PlaylistRESTfulApplication.java
@@ -0,0 +1,13 @@
+package eu.italchemy.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistRESTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistRESTfulApplication.class, args);
+	}
+
+}
